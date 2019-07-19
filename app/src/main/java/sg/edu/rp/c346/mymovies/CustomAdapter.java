9efb5d9405d18c467480cb5a53b773ee17940689
XUMINGLIN <18046506@myrp.edu.sg>
2019-07-19 23:45:12 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/mymovies/CustomAdapter.java
@@ -0,0 +1,51 @@
+package sg.edu.rp.c346.mymovies;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends ArrayAdapter {
+    Context parent_context;
+    int layout_id;
+    ArrayList<Movie>movieList;
+
+    public CustomAdapter(Context context, int resource, ArrayList<Movie>objects){
+        super(context,resource,objects);
+        parent_context = context;
+        layout_id = resource;
+        movieList = objects;
+    }
+
+    @Override
+    public View getView(int position, View converView, ViewGroup parent){
+        LayoutInflater inflater = (LayoutInflater)parent_context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(layout_id, parent, false);
+        TextView tvTitle = rowView.findViewById(R.id.textViewTitle);
+        TextView tvYear = rowView.findViewById(R.id.textViewYear);
+        TextView tvGenre = rowView.findViewById(R.id.textViewGenre);
+        ImageView ivPG = rowView.findViewById(R.id.imageViewPG);
+
+        Movie currentItem = movieList.get(position);
+        tvTitle.setText(currentItem.getTitle());
+        tvYear.setText(currentItem.getYear());
+        tvGenre.setText(currentItem.getGenre());
+
+        if(currentItem.getRated() == "pg"){
+            ivPG.setImageResource(R.drawable.rating_pg);
+        }
+        else if(currentItem.getRated() =="pg13"){
+            ivPG.setImageResource(R.drawable.rating_pg13);
+        }
+
+        return rowView;
+
+    }
+
+}
---
+++ app/src/main/java/sg/edu/rp/c346/mymovies/MainActivity.java
@@ -0,0 +1,53 @@
+package sg.edu.rp.c346.mymovies;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Currency;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lvMovie;
+    ArrayList<Movie>alMovie;
+    CustomAdapter caMovie;
+    LinearLayout bt1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bt1 = findViewById(R.id.bt);
+
+        lvMovie = findViewById(R.id.ListViewMovie);
+        alMovie = new ArrayList<>();
+        Movie item1 = new Movie("The Avengers","2012","pg13"," - Action | Sci-Fi","15/11/2014","Golden Village","Nick Fury of S.H.I.E.L.D. assembles a team of superheroes to save the planet from Loki and his army.",4);
+        alMovie.add(item1);
+        Movie item2 = new Movie("Planes","2013","pg"," - Animation | Comedy","15/5/2015","Cathay - AMK Hub","A crop-dusting plane with a fear of heights lives his dream of competing in a famous around-the-world aerial race.",2);
+        alMovie.add(item2);
+
+        caMovie = new CustomAdapter(this, R.layout.row,alMovie);
+
+
+        lvMovie.setAdapter(caMovie);
+
+
+
+
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent detail = new Intent(MainActivity.this,Details.class);
+                startActivity(detail);
+            }
+        });
+
+    }
+}
